5db4aa0c4f0c512f07c2af6185a27d429e14df39
Bagus Pribadi Setiawan <bsetiawan88@gmail.com>
2019-04-29 12:12:09 +0700

    update

--- src/Odoo.php
+++ src/Odoo.php
@@ -6,12 +6,37 @@ use Ripcord\Ripcord;
 
 class Odoo
 {
+    var $uid;
+    var $password;
+    var $database;
+    var $model;
 
-    public function __construct()
+    public function __construct($username, $password, $database, $url)
     {
         $info = Ripcord::client('https://demo.odoo.com/start')->start();
-        list($url, $db, $username, $password) = array($info['host'], $info['database'], $info['user'], $info['password']);
-        print_r($info);
+        $common = ripcord::client("$url/xmlrpc/2/common");
+        $this->password = $password;
+        $this->database = $database;
+        $this->uid = $common->authenticate($database, $username, $password, array());
+
+        $this->model = ripcord::client("$url/xmlrpc/2/object");
+    }
+
+    public function search($object, $search_params = []) {
+        $this->model->execute_kw($this->database, $this->uid, $this->password,
+            $object, 'search', array(
+                $search_params
+            )
+        );
+
+        return $this->_formatReturn();
+    }
+
+    private function _formatReturn() {
+        $xml = simplexml_load_string($this->model->_response);
+        $json = json_encode($xml);
+        $array = json_decode($json);
+        return $array->params->param->value->array->data->value;
     }
 
 }
